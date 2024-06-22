83e286527e7f2b475b7481bf759910cb9f9efa4c
Vladyslav Zyma <vladzima13@gmail.com>
2024-06-22 10:34:07 +0300

    add tabs v2

--- tabs/script.js
+++ tabs/script.js
@@ -28,3 +28,36 @@ function activateTabs1() {
   });
 }
 activateTabs1();
+
+//===== V1 =====
+// this function just for encapsulation tab v2 code
+function activateTabs2() {
+  const tabsBtnWrapper = document.querySelector('.section2__tabs-btn');
+  const tabBtns = document.querySelectorAll('.section2__tabs-btn-item');
+  const tabContentItems = document.querySelectorAll(
+    '.section2__tabs-content-item',
+  );
+
+  tabsBtnWrapper.addEventListener('click', function (e) {
+    e.stopPropagation();
+    const clickedTab = e.target.closest('.section2__tabs-btn-item');
+    if (!clickedTab) return;
+
+    // removing active classes
+    tabBtns.forEach(btn =>
+      btn.classList.remove('section2__tabs-btn-item--active'),
+    );
+    tabContentItems.forEach(item =>
+      item.classList.remove('section2__tabs-content-item--active'),
+    );
+
+    // activating tab
+    const id = clickedTab.dataset.button;
+
+    clickedTab.classList.add('section2__tabs-btn-item--active');
+    document
+      .getElementById(id)
+      .classList.add('section2__tabs-content-item--active');
+  });
+}
+activateTabs2();
